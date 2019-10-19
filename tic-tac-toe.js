f58e77e8150965cec42a30b69357e9c7a7e402c1
Monique Satchwell <mkayla510@hotmail.com>
2019-10-18 21:06:46 -0500

    Completed Exercise 5 & 6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,10 @@ window.onload =function(){
 	var child= board.children;
 	var xo="X";
 	var state= ["","","","","","","","",""];
+	let btn =document.getElementsByClassName("btn")[0];
+
+	btn.onclick= function(){buttonCLicked()};
+	
 	for( let i=0; i < child.length; i++ ){
 		
 		child[i].classList.add("square");
@@ -25,8 +29,7 @@ window.onload =function(){
 				if(who_won("X")){
 					statusDiv.innerHTML="Congratulations! X is the Winner";
 					statusDiv.classList.add("you-won");
-
-					
+								
 				}
 
 				xo="O";
@@ -38,6 +41,7 @@ window.onload =function(){
 				if(who_won("O")){
 					statusDiv.innerHTML="Congratulations! O is the Winner";
 					statusDiv.classList.add("you-won");
+					
 				}
 				xo="X";
 			}
@@ -52,23 +56,49 @@ window.onload =function(){
 	
 	function who_won(letter){
 		for(let i=0;i<7;i+=3){
-			if (state[i]===letter && state[i+1]===letter && state[i+2]===letter){
+			if ((state[i]===letter) && (state[i+1]===letter) && (state[i+2]===letter)){
+				for( let j=0; j < child.length; j++ ){
+					child[j].onclick = function () {};
+				}
 				return true;
 			}
 		}
 		for(let i=0;i<3;i++){
-			if(state[i]===letter && state[i+3]===letter && state[i+6]===letter){
+			if((state[i]===letter) && (state[i+3]===letter) && (state[i+6]===letter)){
+				for( let j=0; j < child.length; j++ ){
+					child[j].onclick = function () {};
+				}
 				return true;
 			}
 
 		}
 
-		if((state[0]===letter && state[4]===letter && state[8]===letter)  || (state[2]===letter && state[4]===letter && state[6]===letter)){
+		if(((state[0]===letter) && (state[4]===letter) && (state[8]===letter))  || ((state[2]===letter) && (state[4]===letter) && (state[6]===letter))){
+			for( let j=0; j < child.length; j++ ){
+				child[j].onclick = function () {};
+			}
 			return true;
 		}
 		
 		return false; 
 
+	}
+
+	function buttonCLicked() {
+		for(let i=0; i< child.length;i++){
+		
+			state[i] = "";
+			child[i].innerHTML ="";
+			child[i].onclick=function() {onClicked(child[i], i)};
+			child[i].classList.remove("X");
+			child[i].classList.remove("O");
+			
+			
+			
+		
+		}
+		statusDiv.innerHTML="Move your mouse over a square and click to play an X or an O.";
+		statusDiv.classList.remove("you-won");
 	}
 
 
